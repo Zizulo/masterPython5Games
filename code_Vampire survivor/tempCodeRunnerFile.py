97da6f81bb35e0ecd4122ee834b6983c0f681ad7
Zizulo <danieluzytkownik@gmail.com>
2025-03-17 17:05:16 +0100

    Vampire survivor

---
+++ code_Vampire survivor/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+for obj in map.get_layer_by_name('Objects'):
+        #     CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+        
+        # for obj in map.get_layer_by_name('Collisions'):
+        #     InvisibleCollisionSprite((obj.x, obj.y), (self.all_sprites, self.collision_sprites))
