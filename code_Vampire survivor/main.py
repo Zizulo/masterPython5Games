2f5d1f2ee077f1595935ae15512cc0e059d379d8
Zizulo <danieluzytkownik@gmail.com>
2025-03-18 17:08:37 +0100

    update

--- code_Vampire survivor/main.py
+++ code_Vampire survivor/main.py
@@ -3,6 +3,7 @@ from player import Player
 from sprites import *
 from random import randint
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 class Game:
     def __init__(self):
@@ -13,16 +14,14 @@ class Game:
         self.running = True
 
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
-
-        # sprites
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
     
     def setup(self):
         map = load_pygame(join('5games-main', 'Vampire survivor', 'data', 'maps', 'world.tmx'))
+        
         for x, y, image in map.get_layer_by_name('Ground').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
@@ -30,8 +29,13 @@ class Game:
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
         
         for obj in map.get_layer_by_name('Collisions'):
-            # InvisibleCollisionSprite((obj.x, obj.y), (self.all_sprites, self.collision_sprites))
-            print(obj)
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
+
     def run(self):
         while self.running:
             # dt
@@ -44,9 +48,10 @@ class Game:
             
             # update
             self.all_sprites.update(dt)
+
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
--- code_Vampire survivor/sprites.py
+++ code_Vampire survivor/sprites.py
@@ -1,10 +1,12 @@
 from settings import *
+from math import atan2, degrees
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+        self.ground = True
 
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -12,7 +14,33 @@ class CollisionSprite(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
 
-class InvisibleCollisionSprite(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        # player connection
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1, 0)
+
+        # sprite setup
         super().__init__(groups)
-        self.rect = self.get_frect(topleft = pos)
+        self.gun_surf = pygame.image.load(join('5games-main', 'Vampire survivor', 'images', 'gun', 'gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
